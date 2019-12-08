21e63830763095f8c9f8484b936ced532db0026e
Jan Oliver Rüdiger <github@jan-oliver-ruediger.de>
2019-12-08 03:02:55 +0100

    // commit

---
+++ KAMOKO.Game.Client.App/RadForm1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+using Telerik.WinControls.UI;
+
+namespace KAMOKO.Game.Client.App
+{
+  public partial class RadForm1 : Telerik.WinControls.UI.RadForm
+  {
+    public RadForm1()
+    {
+      InitializeComponent();
+    }
+  }
+}
